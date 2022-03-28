8da1a8efe6f7245db811b950646dc85425a9731a
usama farooq <usama.farooq@norig.com>
2022-03-28 11:33:22 +0500

    minor fixes

--- IOS-combine/Common/Constants.swift
+++ IOS-combine/Common/Constants.swift
@@ -33,9 +33,9 @@ struct Constants {
 }
 
 struct AuthenticationConstants {
-    static let PROJECTID = "176GK5IN"
+    static let PROJECTID = "1PHN0E"
     static let AUTHTOKEN = "3d9686b635b15b5bc2d19800407609fa"
-    static let HOST = "tenant-api.vdotok.dev"
+    static let HOST = "d-tenant.vdotok.dev"
 }
 
 struct AppsGroup {
